d85f08bba736735ae039697ae6b66bb07d1d167f
zengwenquan <724708892@qq.com>
2022-03-29 07:38:13 +0800

    feat: 定位

---
+++ js/defaultOption.js
@@ -0,0 +1,18 @@
+const defaultOption = {
+  animation: false,
+  // 隐藏地图选择控件
+  baseLayerPicker: false,
+  // 隐藏全屏控件
+  fullscreenButton: false,
+  // 隐藏地理编码控件
+  geocoder: false,
+  // 隐藏
+  homeButton: false,
+  infoBox: false,
+  sceneModePicker: false,
+  selectionIndicator: false,
+  timeline: false,
+  navigationHelpButton: false,
+  navigationInstructionsInitiallyVisible: false,
+  scene3DOnly: true,
+};
